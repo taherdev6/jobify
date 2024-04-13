a15235c7510e78b4a9edf9b0b309bdace9b2074e
taherdev6 <taherdev6@gmail.com>
2024-04-13 22:43:29 +0300

    fixed image upload

--- controller/userController.js
+++ controller/userController.js
@@ -2,7 +2,7 @@ import { StatusCodes } from "http-status-codes";
 import Job from "../models/JobModel.js";
 import User from "../models/UserModel.js";
 import cloudinary from "cloudinary";
-import { promises as fs } from "fs";
+import { formatImage } from "../middleware/multerMiddleware.js";
 
 const getCurrentUser = async (req, res) => {
   const user = await User.findById(req.user.userId);
@@ -24,8 +24,10 @@ const updateUser = async (req, res) => {
   }
 
   if (req.file) {
-    const response = await cloudinary.v2.uploader.upload(req.file.path);
-    await fs.unlink(req.file.path);
+    const file = formatImage(req.file);
+
+    const response = await cloudinary.v2.uploader.upload(file);
+
     newUser.avatar = response.secure_url;
     newUser.avatarPublicId = response.public_id;
   }
